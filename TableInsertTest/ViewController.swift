b387e3c2bfa8abef6fc1c8e6e5f4e8dbf8325cb2
Niklas Saers <niklas@saers.com>
2016-02-15 08:29:55 +0100

    Reproduce insertSection problem

--- TableInsertTest/ViewController.swift
+++ TableInsertTest/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var numSections = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tableView.beginUpdates()
+        numSections += 1
+        tableView.insertSections(NSIndexSet(index:0), withRowAnimation: .Automatic)
+        numSections += 1
+        tableView.insertSections(NSIndexSet(index:0), withRowAnimation: .Automatic)
+        numSections += 1
+        tableView.insertSections(NSIndexSet(index:0), withRowAnimation: .Automatic)
+        numSections += 1
+        tableView.insertSections(NSIndexSet(index:0), withRowAnimation: .Automatic)
+        numSections += 1
+        tableView.insertSections(NSIndexSet(index:0), withRowAnimation: .Automatic)
+        tableView.endUpdates()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +44,24 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UITableViewDelegate {
+    
+    
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Cell (\(indexPath.section),\(indexPath.row))"
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return numSections
+    }
+    
+}
