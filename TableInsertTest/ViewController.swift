0ba862fb97635cfe252fa45a38775c4fb2defcff
Niklas Saers <niklas@saers.com>
2016-02-15 08:01:07 +0100

    Initial Commit

---
+++ TableInsertTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableInsertTest
+//
+//  Created by Niklas Saers on 15/02/16.
+//  Copyright © 2016 Snapsale AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
